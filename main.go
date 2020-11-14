a5a188aa79f6864ab2258b7be17e0d5720f1f383
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-14 17:43:55 +0700

    create struct book model

--- main.go
+++ main.go
@@ -6,6 +6,14 @@ import (
 	"net/http"
 )
 
+// model book
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
 func main() {
 	// init route
 	//r := mux.NewRouter()
