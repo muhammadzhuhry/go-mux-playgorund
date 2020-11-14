28a3f3fdfc6adf5e6fe2138474b12bbd990b23a0
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-14 17:25:37 +0700

    create basic server

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("existing branch list:")
-	fmt.Println("go-mux-basic -> reference by traversy media tutorial")
+	// init route
+	//r := mux.NewRouter()
+	var r *mux.Router = mux.NewRouter()
+
+	// route hanlders / endpoints
+	//r.HandleFunc("/api/books", getBooks).Methods("GET")
+	//r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	//r.HandleFunc("/api/book", createBook).Methods("POST")
+	//r.HandleFunc("/api/book/{id}", updateBook).Methods("PUT")
+	//r.HandleFunc("/api/book/{id}", deleteBook).Methods("DELETE")
+
+	// start server
+	log.Fatal(http.ListenAndServe(":9000", r))
 }
