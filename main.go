1be52f280d06f3243d6a06eb77d45cb454dd145a
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-14 20:18:10 +0700

    remove unused comment

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
-	json.NewDecoder(r.Body).Decode(&book) // change into book later
+	json.NewDecoder(r.Body).Decode(&book)
 	book.ID = strconv.Itoa(rand.Intn(100000))
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
@@ -67,7 +67,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 
 			// insert the new one
 			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book) // change into book later
+			_ = json.NewDecoder(r.Body).Decode(&book)
 			book.ID = param["id"]
 			books = append(books, book)
 			json.NewEncoder(w).Encode(book)
