ef305c592e2f5ea1f48677366f5caad92831d49f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-15 11:27:41 +0700

    modify main.go

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("existing branch list:")
 	fmt.Println("go-mux-basic -> reference by traversy media tutorial")
+	fmt.Println("go-mux-mongodb -> reference by the polyglot dev tutorial")
 }
