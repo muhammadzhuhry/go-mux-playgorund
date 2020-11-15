8bea3c767f833621fd0b3fa1c5529c7afe4cb547
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-11-15 12:16:18 +0700

    create function crete person

--- main.go
+++ main.go
@@ -2,24 +2,42 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
 	"time"
 )
 
 type Person struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
-	Lastname  string             `json:"lastname,omitempty" bson:"lasttname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
 }
 
 var client *mongo.Client
+
+func CreatePerson(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var person Person
+	json.NewDecoder(request.Body).Decode(&person)
+
+	collection := client.Database("godbtest").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, person)
+
+	json.NewEncoder(response).Encode(result)
+}
 
 func main() {
 	fmt.Println("Starting the application...")
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	router := mux.NewRouter()
+	http.ListenAndServe(":9000", router)
 }
